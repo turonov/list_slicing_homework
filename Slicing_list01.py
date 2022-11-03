6eaa529830140727c1fcad4e33273acb94cc01a6
turonov <turonov5400gmail.com>
2022-11-03 21:53:31 +0500

    01

--- Slicing_list01.py
+++ Slicing_list01.py
@@ -5,5 +5,7 @@ def main(numbers):
         numbers(list): parameter
     Returns:
         list: return answer.
-    """
-    return
+    """ 
+    return numbers[::2] 
+
+print(main([1,2,3,4,5,6,7]))
